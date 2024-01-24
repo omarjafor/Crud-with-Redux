63b59d43f8b849fe105a9f121b87feebd89ca380
omarjafor <omarjaforchy@gmail.com>
2024-01-24 18:24:29 +0600

    redux tolkit crud starting

---
+++ src/Root.jsx
@@ -0,0 +1,13 @@
+
+
+function Root() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">Crud with Redux Tolkit</h1>
+      </div>
+    </>
+  )
+}
+
+export default Root;
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Root from './Root'
+import { RouterProvider } from 'react-router-dom'
+import Router from './Router'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={Router}>
+      <Root />
+    </RouterProvider>
+  </React.StrictMode>,
+)
