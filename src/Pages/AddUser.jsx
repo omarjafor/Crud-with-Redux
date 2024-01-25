de7a6383c758cc57f6c32866181efd78c4bf986e
omarjafor <omarjaforchy@gmail.com>
2024-01-25 11:59:09 +0600

    redux store and user add user

--- src/Pages/AddUser.jsx
+++ src/Pages/AddUser.jsx
@@ -1,9 +1,38 @@
+import { useState } from "react";
+import TextField from "../Components/TextField";
+import Button from "../Components/Button";
+import { useNavigate } from "react-router-dom";
 
 
 const AddUser = () => {
+    const navigate = useNavigate();
+    const [values, setValues] = useState({
+        name: '',
+        email: ''
+    });
+
+    const handleAddUser = () => {
+        setValues({ name: '', email: '' });
+        
+        navigate('/');
+    }
+
     return (
-        <div>
-            ADD USers
+        <div className="mt-10 max-w-xl mx-auto">
+            <TextField
+                label="Name"
+                value={values.name}
+                onChange={(e) => setValues({ ...values, name: e.target.value })}
+                inputProps={{ type: 'text', placeholder: 'Jhon Doe' }}
+            />
+            <br />
+            <TextField
+                label="Email"
+                value={values.email}
+                onChange={(e) => setValues({ ...values, email: e.target.value })}
+                inputProps={{ type: 'email', placeholder: 'jhondoe@mail.com' }}
+            />
+            <Button onClick={handleAddUser}>Submit</Button>
         </div>
     );
 };
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -13,8 +13,8 @@ const Home = () => {
         console.log(id);
     }
 
-    const renderCard = () => users.map((user, id) => (
-        <div className="bg-gray-300 p-5 flex items-center justify-between" key={id}>
+    const renderCard = () => users.map(user => (
+        <div className="bg-gray-300 p-5 flex items-center justify-between" key={user.id}>
             <div>
                 <h3 className="font-bold text-lg text-gray-700">{user.name}</h3>
                 <span className="font-normal text-gray-600">{user.email}</span>
